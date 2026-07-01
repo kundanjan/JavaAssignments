ca755fef935a3ec68b0676ca9b8a1cf69d4886b7
kundanjan <kundanjangale@kundanjangale>
2026-07-01 08:52:30 +0530

    IPL Project (Assignment) Improvements : Used CSV data file for data (ipl-data.csv) through IPLDataProvider, Used Stream.

--- Assignments/src/com/mmcoe/assignment5/IPLDataProvider.java
+++ Assignments/src/com/mmcoe/assignment5/IPLDataProvider.java
@@ -1,55 +1,31 @@
 package com.mmcoe.assignment5;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
-import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class IPLDataProvider {
 	public static void loadData(IPLService service) {
-		service.addTeam(new Team("CSK"), Arrays.asList(
-			new Player("MS Dhoni", 7, "WK", "Indian", 12),
-			new Player("Ruturaj Gaikwad", 31, "Batsman", "Indian", 7),
-			new Player("Deepak Chahar", 90, "Bowler", "Indian", 4),
-			new Player("Moeen Ali", 18, "All-rounder", "English", 6),
-			new Player("Shivam Dube", 25, "All-rounder", "Indian", 3.5),
-			new Player("Tushar Deshpande", 8, "Bowler", "Indian", 2),
-			new Player("Devon Conway", 88, "Batsman", "Kiwi", 6.5)
-		));
+		try {
+			Map<String, List<Player>> teamPlayers = Files.lines(Paths.get("src/com/mmcoe/assignment5/ipl-data.csv"))
+				.skip(1)
+				.map(line -> line.split(","))
+				.collect(Collectors.groupingBy(
+					record -> record[0],
+					Collectors.mapping(
+						record -> new Player(record[1], Integer.parseInt(record[2]), record[3], record[4], Double.parseDouble(record[5])),
+						Collectors.toList()
+					)
+				));
 
-		service.addTeam(new Team("MI"), Arrays.asList(
-			new Player("Rohit Sharma", 45, "Batsman", "Indian", 15),
-			new Player("Suryakumar Yadav", 63, "Batsman", "Indian", 10),
-			new Player("Jasprit Bumrah", 93, "Bowler", "Indian", 12),
-			new Player("Ishan Kishan", 23, "WK", "Indian", 8),
-			new Player("Tim David", 85, "Batsman", "Australian", 5.5),
-			new Player("Piyush Chawla", 6, "Bowler", "Indian", 2),
-			new Player("Tilak Varma", 9, "Batsman", "Indian", 3)
-		));
-
-		service.addTeam(new Team("RCB"), Arrays.asList(
-			new Player("Virat Kohli", 18, "Batsman", "Indian", 17),
-			new Player("Faf du Plessis", 13, "Batsman", "South African", 8),
-			new Player("Glenn Maxwell", 32, "All-rounder", "Australian", 9),
-			new Player("Mohammed Siraj", 77, "Bowler", "Indian", 5),
-			new Player("Dinesh Karthik", 1, "WK", "Indian", 4.5),
-			new Player("Wanindu Hasaranga", 5, "All-rounder", "Sri Lankan", 7)
-		));
-
-		service.addTeam(new Team("KKR"), Arrays.asList(
-			new Player("Shreyas Iyer", 41, "Batsman", "Indian", 10),
-			new Player("Andre Russell", 12, "All-rounder", "West Indian", 8),
-			new Player("Sunil Narine", 74, "All-rounder", "West Indian", 6),
-			new Player("Rinku Singh", 35, "Batsman", "Indian", 4),
-			new Player("Varun Chakravarthy", 29, "Bowler", "Indian", 5),
-			new Player("Nitish Rana", 27, "All-rounder", "Indian", 3.5)
-		));
-
-		service.addTeam(new Team("RR"), Arrays.asList(
-			new Player("Sanju Samson", 11, "WK", "Indian", 10),
-			new Player("Jos Buttler", 17, "WK", "English", 7),
-			new Player("Yashasvi Jaiswal", 19, "Batsman", "Indian", 3),
-			new Player("Ravichandran Ashwin", 99, "All-rounder", "Indian", 5),
-			new Player("Shimron Hetmyer", 3, "Batsman", "West Indian", 6.5),
-			new Player("Trent Boult", 18, "Bowler", "Kiwi", 6)
-		));
+			teamPlayers.forEach((teamName, players) ->
+				service.addTeam(new Team(teamName), players)
+			);
+		} catch (IOException e) {
+			System.out.println("Error reading CSV: " + e.getMessage());
+		}
 	}
 }
